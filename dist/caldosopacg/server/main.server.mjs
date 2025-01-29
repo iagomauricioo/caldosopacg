bcd322ea1f22478868f56565047530fb6a657dd1
Iago Mauricio <iagomauricio7@gmail.com>
2025-01-29 14:21:56 -0300

    feito copiar link

--- dist/caldosopacg/server/main.server.mjs
+++ dist/caldosopacg/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-6MYWIAY4.mjs";import"./chunk-K656ALOT.mjs";import"./chunk-TSHQNZ6W.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-RJLPFAIQ.mjs";import"./chunk-MO5GRPP5.mjs";import"./chunk-BG7ITQEO.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,16 +1,34 @@
 import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { DialogMenuComponent } from '../dialog-menu/dialog-menu.component';
 import { MatDialog } from '@angular/material/dialog';
-
+import { TooltipPosition, MatTooltipModule } from '@angular/material/tooltip';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CdkScrollable} from '@angular/cdk/scrolling';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatExpansionModule, DialogMenuComponent],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatExpansionModule,
+    DialogMenuComponent,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CdkScrollable,
+    MatButtonModule,
+    MatTooltipModule,
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,5 +38,28 @@ export class NavComponent {
 
   openDialog() {
     this.dialog.open(DialogMenuComponent);
+  }
+
+  position = new FormControl('below');
+
+  private _snackBar = inject(MatSnackBar);
+
+  message = 'Link copiado com sucesso!';
+  action = 'Fechar';
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action);
+  }
+
+  copyLink(): void {
+    const link = window.location.href;
+    navigator.clipboard
+      .writeText(link)
+      .then(() => {
+        console.log('Link copiado com sucesso!');
+      })
+      .catch((err) => {
+        console.error('Erro ao copiar o link: ', err);
+      });
   }
 }
