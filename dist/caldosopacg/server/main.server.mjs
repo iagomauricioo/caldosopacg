6e30f02333208bd8bf42c08a2c9d07c2f73a03c7
Iago Mauricio <iagomauricio7@gmail.com>
2025-01-29 11:31:57 -0300

    resolvendo bug do link cardapio

--- dist/caldosopacg/server/main.server.mjs
+++ dist/caldosopacg/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-FGPCH4GB.mjs";import"./chunk-JSPWMZYP.mjs";import"./chunk-IPVXWTD6.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-FHGNTYZB.mjs";import"./chunk-JSPWMZYP.mjs";import"./chunk-IPVXWTD6.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
