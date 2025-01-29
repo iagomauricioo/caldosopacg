abbbe0644547e7d22605c64975b23a5f563334ba
Iago Mauricio <iagomauricio7@gmail.com>
2025-01-29 13:14:51 -0300

    tudo funcionando até o momento

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -11,5 +11,47 @@ import {MatCardModule} from '@angular/material/card';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent {
-
+  menuItems = [
+    {
+      name: "Caldo de Frango",
+      options: [
+        { size: "350ml", price: 12.00 },
+        { size: "500ml", price: 15.00 }
+      ]
+    },
+    {
+      name: "Caldo de Frango, bacon e calabresa",
+      options: [
+        { size: "350ml", price: 13.00 },
+        { size: "500ml", price: 16.00 }
+      ]
+    },
+    {
+      name: "Caldo de Charque",
+      options: [
+        { size: "350ml", price: 15.00 },
+        { size: "500ml", price: 18.00 }
+      ]
+    },
+    {
+      name: "Caldo de Feijão",
+      options: [
+        { size: "350ml", price: 10.00 },
+        { size: "500ml", price: 13.00 }
+      ]
+    },
+    {
+      name: "Caldo de Camarão",
+      options: [
+        { size: "350ml", price: 15.00 },
+        { size: "500ml", price: 18.00 }
+      ]
+    },
+    {
+      name: "Sopa",
+      options: [
+        { size: "900ml", price: 18.00 }
+      ]
+    }
+  ];
 }
