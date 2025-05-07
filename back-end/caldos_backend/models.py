cdc55c2cb18d2ada2354cd10f5e58493533a985e
Iago Mauricio <iagomauricio7@gmail.com>
2025-05-07 12:27:07 -0300

    começando módulo de produtos

--- back-end/caldos_backend/models.py
+++ back-end/caldos_backend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField()
+    preco_em_centavos = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
